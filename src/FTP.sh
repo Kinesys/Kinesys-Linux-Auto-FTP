5091ff5c318e9fc7d126692d9bd5bd437a8ea994
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-10-29 15:35:35 +0900

    Update FTP.sh

--- src/FTP.sh
+++ src/FTP.sh
@@ -1,15 +1,17 @@
 #Kinesys Linux Auto FTP.sh
-export Today = "`date '+%y-%m-%d'`"
-
+export Today = "`date '+%y-%m-%d'`" #시간 출력
+export day = `date+%a` #요일 출력
 address = "FTP Ip Address"
 
 id = 'FTP ID'
 passwd = 'FTP PW'
-
+#passive모드로 작동함
 ftp -n -v $address << EOF
 
 user $id $passwd
+ifconfig
 cd /
+passive 
 prompt
 bi
 mkdir $Today
